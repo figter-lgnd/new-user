f79405fde7a0abffc4bbd3665b1c89416b3a088f
aitozoldyck <60502277+aitozoldyck@users.noreply.github.com>
2020-02-02 12:31:49 +0000

    Update kaitoxdlogs.py

--- stdplugins/kaitoxdlogs.py
+++ stdplugins/kaitoxdlogs.py
@@ -4,4 +4,4 @@
 async def handler(event):
     # check media type
     if event.audio:
-    	await borg.forward_messages("@songsxd", event.message)
+    	await borg.forward_messages("@songsxd", event.message)'''
