b80bf876735f35b541662f293797add49c62fc79
aitozoldyck <60502277+aitozoldyck@users.noreply.github.com>
2020-02-02 14:14:40 +0000

    Update kaitolog01.py

--- stdplugins/kaitolog01.py
+++ stdplugins/kaitolog01.py
@@ -1,7 +1,7 @@
-from telethon import events
+'''from telethon import events
 
 @borg.on(events.NewMessage(chats=-1001128441021))
 async def handler(event):
     
     
-    	await borg.forward_messages("@dwnmp3Bot", event.message)
+    	await borg.forward_messages("@dwnmp3Bot", event.message)'''
