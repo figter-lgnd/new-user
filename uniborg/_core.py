f6044b35052ae74536395e0829ccb2f62db3c092
aitozoldyck <60502277+aitozoldyck@users.noreply.github.com>
2020-02-04 20:09:24 +0530

    Update _core.py

--- uniborg/_core.py
+++ uniborg/_core.py
@@ -20,7 +20,7 @@ async def load_reload(event):
         if shortname in borg._plugins:  # pylint:disable=E0602
             borg.remove_plugin(shortname)  # pylint:disable=E0602
         borg.load_plugin(shortname)  # pylint:disable=E0602
-        msg = await event.respond(f"Successfully (re)loaded plugin {shortname}")
+        msg = await event.respond(f" Successfully (re)load plugin {shortname}")
         await asyncio.sleep(DELETE_TIMEOUT)
         await msg.delete()
     except Exception as e:  # pylint:disable=C0103,W0703
@@ -40,7 +40,7 @@ async def remove(event):
         borg.remove_plugin(shortname)  # pylint:disable=E0602
         msg = await event.respond(f"Removed plugin {shortname}")
     else:
-        msg = await event.respond(f"Plugin {shortname} is not loaded")
+        msg = await event.respond(f"plugin {shortname} is not load...")
     await asyncio.sleep(DELETE_TIMEOUT)
     await msg.delete()
 
@@ -62,27 +62,27 @@ async def send_plug_in(event):
     )
     end = datetime.now()
     time_taken_in_ms = (end - start).seconds
-    await event.edit("Uploaded {} in {} seconds".format(input_str, time_taken_in_ms))
+    await event.edit("plugin is upload {} in {} seconds".format(input_str, time_taken_in_ms))
     await asyncio.sleep(DELETE_TIMEOUT)
     await event.delete()
 
 
-@borg.on(util.admin_cmd(pattern="install",allow_sudo=True))  # pylint:disable=E0602
+@borg.on(util.admin_cmd(pattern="install plugin"))  # pylint:disable=E0602
 async def install_plug_in(event):
     if event.fwd_from:
         return
     if event.reply_to_msg_id:
         try:
-            downloaded_file_name = await event.client.download_media(
+            downloaded_file_name = await event.client.download_media(  # pylint:disable=E0602
                 await event.get_reply_message(),
-                borg.n_plugin_path  # pylint:disable=E0602
+                borg._plugin_path  # pylint:disable=E0602
             )
             if "(" not in downloaded_file_name:
                 borg.load_plugin_from_file(downloaded_file_name)  # pylint:disable=E0602
-                await event.edit("Installed Plugin `{}`".format(os.path.basename(downloaded_file_name)))
+                await event.edit(" Instulled Plugin `{}`".format(os.path.basename(downloaded_file_name)))
             else:
                 os.remove(downloaded_file_name)
-                await event.edit("Errors! Cannot install this plugin.")
+                await event.edit("Plugin is not instaling 😕.")
         except Exception as e:  # pylint:disable=C0103,W0703
             await event.edit(str(e))
             os.remove(downloaded_file_name)
