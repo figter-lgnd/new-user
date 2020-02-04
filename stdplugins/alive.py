6e559f137dc809fd6b79f4a0744bb7159ce52137
aitozoldyck <60502277+aitozoldyck@users.noreply.github.com>
2020-02-04 18:02:54 +0530

    Update alive.py

--- stdplugins/alive.py
+++ stdplugins/alive.py
@@ -5,7 +5,7 @@ from telethon.tl.types import ChannelParticipantsAdmins
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd(pattern="alive"))
+@borg.on(admin_cmd(pattern="alive", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
