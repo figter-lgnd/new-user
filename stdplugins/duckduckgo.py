2ea447c4505b97cd2bbd56f5d417c0a49150ed95
aitozoldyck <60502277+aitozoldyck@users.noreply.github.com>
2020-02-05 16:59:46 +0530

    for all users

--- stdplugins/duckduckgo.py
+++ stdplugins/duckduckgo.py
@@ -1,3 +1,10 @@
+"""Duck Duck Go  
+For all users
+Syntax: .sudoud Query
+customized by @meanii 
+Please Don't remove credit name 
+"""
+
 from telethon import events
 import os
 import requests
@@ -5,14 +12,15 @@ import json
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("ddg (.*)"))
+@borg.on(events.NewMessage(pattern=r"\.ddg (.*)",incoming=True))
 async def _(event):
     if event.fwd_from:
         return
+    
     input_str = event.pattern_match.group(1)
     sample_url = "https://duckduckgo.com/?q={}".format(input_str.replace(" ","+"))
     if sample_url:
         link = sample_url.rstrip()
-        await event.edit("Let me 🦆 DuckDuckGo that for you:\n🔎 [{}]({})".format(input_str, link))
+        await event.reply("Let me 🦆 DuckDuckGo that for you:\n🔎 [{}]({})".format(input_str, link))
     else:
-        await event.edit("something is wrong. please try again later.")
+        await event.reply("something is wrong. please try again later.")
