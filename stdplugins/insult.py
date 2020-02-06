0fcaa799c3239267129a0952087844944dff4727
Anil Chauhan <60502277+anilchauhanxda@users.noreply.github.com>
2020-02-06 14:50:22 +0530

    for all user

--- stdplugins/insult.py
+++ stdplugins/insult.py
@@ -1,7 +1,13 @@
-# For UniBorg
-# By Priyam Kalra
-# Based on the insult module made by Hackintosh for friendly telegram bot (https://da.gd/RG2hfe)
-# Syntax (.insult <no_of_times_to_insult>)
+"""
+Syntax:
+.insult(with reply user message)
+For for Users only
+customized by @meanii 
+Please Don't remove credit name 
+
+"""
+
+
 from telethon import events
 from uniborg.util import admin_cmd
 import asyncio
@@ -11,26 +17,21 @@ from sql_helpers.global_variables_sql import LOGGER, SUDO_USERS
 import sys
 import time
 
-"""
-insult: "\
-**Requested Module --> insult**\
-\n\n**Detailed usage of fuction(s):**\
-\n\n```.insult <optional_number_of_insults>``` [optionally as a reply to target user][default = 5]\
-\nUsage: Insults target user.\ """
 
 @borg.on(admin_cmd(pattern="insult ?(.*)"))
+@borg.on(events.NewMessage(pattern=r"\.insult(.*)",incoming=True))
 async def _(event):
     if event.fwd_from:
         return
     args = event.pattern_match.group(1)
-    adjectives_start = ["salty", "fat", "fucking", "shitty",
+    adjectives_start = ["salty", "fat", "f*cking", "sh*tty",
                         "stupid", "retarded", "self conscious", "tiny"]
     adjectives_mid = ["little", "vitamin D deficient",
                       "idiotic", "incredibly stupid"]
-    nouns = ["cunt", "pig", "pedophile", "beta male", "bottom", "retard", "ass licker", "cunt nugget",
-             "PENIS", "dickhead", "flute", "idiot", "motherfucker", "loner", "creep"]
-    starts = ["You're a", "You", "Fuck off you", "Actually die you", "Listen up you",
-              "What the fuck is wrong with you, you"]
+    nouns = ["cunt", "pig", "pedophile", "beta male", "bottom", "retard", "a*s licker", "cunt nugget",
+             "P*NIS", "d*ckhead", "flute", "idiot", "m*therf*cker", "loner", "creep"]
+    starts = ["You're a", "You", "F*ck off you", "Actually die you", "Listen up you",
+              "What the f*ck is wrong with you, you"]
     ends = ["!!!!", "!", ""]
     log_insults = ""
     if args:
@@ -58,16 +59,14 @@ async def _(event):
             user_id = "Unknown user"
             noformat_userid = "Unknown user"
         if noformat_userid in SUDO_USERS:
-            await event.edit("**Wait! WHAT?!\nDid you just try to insult my creator?!?!\nBYE!**")
+            await event.reply("**Wait! WHAT?!\nDid you just try to insult my creator?!?!\nBYE!**")
             sys.exit()
             # probably not needed but meh
             break
         else:
-            await event.edit(insult)
+            await event.reply(insult)
             time.sleep(2)
         await borg.send_message(
             LOGGER,
             f"Insulted [{user_id}] with:\n\n{log_insults}"
        )
-       
-       
